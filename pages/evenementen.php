26a92fbc507f9fa766ebe11d4d64ba5de23887e2
kw1c-VosRomeo <romeo.vos@edu-kw1c.nl>
2022-06-28 09:45:45 +0200

    navbar

--- pages/evenementen.php
+++ pages/evenementen.php
@@ -1,1 +1,9 @@
 <?php
+?>
+<link rel="stylesheet" href="../styles/stylesheetEV.css">
+<ul>
+    <li><a href="/">evenementen</a></li>
+    <li><a href="/">games</a></li>
+    <li><a href="/">teams</a></li>
+    <li style="float:right"><a href="/">login</a></li>
+</ul>
