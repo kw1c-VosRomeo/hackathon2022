7dba8ac35072b64040f1463def37ab159506d7ce
kw1c-VestersLuuk <luuk.vesters@edu-kw1c.nl>
2022-06-28 09:56:01 +0200

    gradient background

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel='stylesheet' href="styles/stylesheet.css">
+        <link rel="stylesheet" href="styles/stylesheetnavbar.css">
         <title>
             Project Zero | Home
         </title>
