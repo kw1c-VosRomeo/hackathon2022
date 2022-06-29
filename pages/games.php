bbed298b1a9a4e12635800af10fbccb7a9bf4fa0
kw1c-VosRomeo <romeo.vos@edu-kw1c.nl>
2022-06-29 09:38:48 +0200

    Romeo

--- pages/games.php
+++ pages/games.php
@@ -19,5 +19,65 @@
     <li><a href="../pages/teams.php">teams</a></li>
 </ul>
 
+
+<p>
+    <img src="../images/csgo.png" alt="csgo">
+
+</p>
+
+<p>
+    <img src="../images/fortnite.jpg" alt="fortnite">
+
+</p>
+
+<p>
+    <img src="../images/LoL.jpg" alt="LoL">
+
+</p>
+
+<p>
+    <img src="../images/mariokart.jpg" alt="mariokart">
+
+</p>
+
+<p>
+    <img src="../images/minecraft.jpg" alt="minecraft">
+
+</p>
+
+<p>
+    <img src="../images/rocketleague.jpg" alt="rocketleague">
+
+</p>
+
+<p>
+    <img src="../images/siege.jpg" alt="siege">
+
+</p>
+
+<p>
+    <img src="../images/fifa22.jfif" alt="fifa22">
+
+</p>
+
+<p>
+    <img src="../images/mortalkombat.jpg" alt="mortalkombat">
+
+</p>
+
+<p>
+    <img src="../images/apexlegends.jfif" alt="apexlegends">
+
+</p>
+
+<p>
+    <img src="../images/vampireTM.jpg" alt="vampireTM">
+
+</p>
+
+<p>
+    <img src="../images/callofduty.jfif" alt="callofduty">
+
+</p>
 </body>
 </html>
